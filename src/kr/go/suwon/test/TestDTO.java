2082a1a8a84c3cc073dde28e825e8d013d94304c
eo1v <halcyonlife@naver.com>
2022-11-21 09:04:15 +0900

    image x / impressList, online, qna yet

---
+++ src/kr/go/suwon/test/TestDTO.java
@@ -0,0 +1,26 @@
+package kr.go.suwon.test;
+public class TestDTO {
+	private String name;
+	private int point;
+	public TestDTO(){ }
+	public TestDTO(String name, int point) {
+		this.name = name;
+		this.point = point;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	@Override
+	public String toString() {
+		return "TestDTO [name=" + name + ", point=" + point + "]";
+	}
+}
